3dd036082a6acb7de43b9112714ca2f60ef83039
Dylan Lim <dylanlim2035@gmail.com>
2025-08-05 14:01:47 +1200

    Added recipe class

--- Recipe.py
+++ Recipe.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+class Recipe:
+    def init(self, id, name, author, description, ingredients_list,
+                 prep_instructions, cook_time):
+
+        self._id = id
+        self._name = name
+        self._author = author
+        self._description = description
+        self._ingredients_list = ingredients_list
+        self._prep_instructions = prep_instructions
+        self._cook_time = cook_time
