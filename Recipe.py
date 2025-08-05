967b48ebbe65b00a9a3dfd4edbe05dd2c52f20c7
Dylan Lim <dylanlim2035@gmail.com>
2025-08-05 13:51:29 +1200

    Test

--- Recipe.py
+++ Recipe.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+class Student:
+    def __init__(self, name,upi):
+        self.name = name
+        self.upi = upi
+    def get_name(self):
+        return self.name
+    def set_name(self,name):
+        self.name = self.name
+
