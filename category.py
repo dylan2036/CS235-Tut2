a84a7d8118e2f7dfa6a1bdd3d828679189ad7932
Daniel Chen <dche391@aucklanduni.ac.nz>
2025-08-05 14:23:04 +1200

    finalised category

--- category.py
+++ category.py
@@ -1,9 +1,19 @@
 class Category:
-    def __init__(self,name, id, recipe):
-        self.__name = name
+    def __init__(self, id, name):
         self.__id = id
-        self.__recipe = []
+        self.__name = name
+        self.__recipes = []
 
+    def add_recipe(self, recipe):
+        if recipe not in self.__recipes:
+            self.__recipes.append(recipe)
+            # Add the reverse link if Recipe supports it
+            if hasattr(recipe, "add_category"):
+                recipe.add_category(self)
 
+    def __str__(self):
+        return f"<Category {self.__id}: {self.__name}>"
 
-
+    def list_recipes(self):
+        recipe_strings = [str(recipe) for recipe in self.__recipes]
+        return f"Category {self.__name} has the following associated recipes: [{', '.join(recipe_strings)}]"
